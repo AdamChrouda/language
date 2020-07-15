35d155cff5fcafb003d9acbca3489df572ac2ae8
AdamChrouda <65009652+AdamChrouda@users.noreply.github.com>
2020-07-15 20:13:40 +0100

    Add files via upload

---
+++ dom project/images_defilantes.js
@@ -0,0 +1,12 @@
+var i = 0;
+var images = [];
+var savon = [];
+var time = 2650
+
+//list of images
+images[0] = 'images/image1.jpg';
+images[1] = 'images/image2.jpg';
+images[2] = 'images/image3.jpg';
+images[3] = 'images/image4.jpg';
+images[4] = 'images/image5.jpg';
+
